c0731952773a483114d8fca26e24ee7ef65e5a78
Thabiso Makolana <makolanathabiso@gmail.com>
2020-11-24 06:35:56 +0200

    changed output of interests into a proper english string output

--- src/person.js
+++ src/person.js
@@ -1,13 +1,21 @@
 class Person {
-    constructor(name,age,gender,interests){
-        this.name=name
-        this.age=age
-        this.gender=gender
-        this.interests=interests
+    constructor(name, age, gender, interests) {
+        this.name = name
+        this.age = age
+        this.gender = gender
+        this.interests = interests
     }
 
-    hello(){
-        return `Hello, my name is ${this.name} and I am ${this.age} years old. My interests are ${this.interests}`
+    stringInterests() {
+        let copy = [...this.interests]
+        let last = copy.pop()
+        let string = copy.join(", ")
+
+        return string + " and " + last
+    }
+
+    hello() {
+        return `Hello, my name is ${this.name} and I am ${this.age} years old. My interests are ${this.stringInterests()}`
     }
 }
 
